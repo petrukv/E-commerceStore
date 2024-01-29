f526b396f01afabf94835e6e0f7d9ef008d23866
Vadym <petrukvadd@gmail.com>
2024-01-29 17:49:14 +0200

    Implemented sessions

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls'))
+    path('', include('store.urls')),
+    path('cart/', include('cart.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
