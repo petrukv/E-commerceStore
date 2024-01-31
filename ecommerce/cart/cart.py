59a95dd1467c6f5f78936ec1a5a0185d58653aa6
Vadym <petrukvadd@gmail.com>
2024-01-31 19:40:56 +0200

    Implemented Delete and Update functionality

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -42,3 +42,20 @@ class Cart():
 
     def get_total(self):
         return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+    
+    def delete(self, product):
+        product_id = str(product)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
+
+    def update(self, product, qty):
+        product_id = str(product)
+        product_quantity = qty
+
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = product_quantity
+
+            self.session.modified = True
