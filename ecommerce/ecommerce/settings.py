b102e89ed6bcd1936e2284c8da727d732b2dba7f
Vadym <petrukvadd@gmail.com>
2024-02-01 23:53:00 +0200

    setup the email backend

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -147,3 +147,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'store', 'static', 'ecommerce', 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = 'onetime96gmail.com'
+EMAIL_HOST_PASSWORD = 'ASDZXCSFAHGEDFSCV'
