2cae9516013c2008c9dc95af9ff5f6df11c5d38c
Vadym <petrukvadd@gmail.com>
2024-02-02 18:11:04 +0200

    updated verification info

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -151,5 +151,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = 'True'
 
-EMAIL_HOST_USER = 'onetime96gmail.com'
-EMAIL_HOST_PASSWORD = 'ASDZXCSFAHGEDFSCV'
+EMAIL_HOST_USER = 'verificdjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'superuser123'
