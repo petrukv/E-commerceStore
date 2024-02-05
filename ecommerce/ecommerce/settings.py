e17e9b77120544516fe606aab2171afe130623ec
Vadym <petrukvadd@gmail.com>
2024-02-05 23:43:54 +0200

    Implemented resetting password

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -146,10 +146,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'store', 'static', 'ecommerce', 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'account.backend.email_backend.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_USE_TLS = 'False'
+EMAIL_USE_TLS = 'True'
 
 EMAIL_HOST_USER = 'testdjangoooo@gmail.com' # - Enter your GMAIL address # The host email that sends password reset emails
 EMAIL_HOST_PASSWORD = 'oilpxjpvtuvgeucu' # - Enter your app password
