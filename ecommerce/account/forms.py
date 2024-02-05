e2aa78e0794982dd53ce7a1bd58464daf8e3f1fc
Vadym <petrukvadd@gmail.com>
2024-02-06 00:23:32 +0200

    Fixed code. Now messages are being sent to the email

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -17,13 +17,10 @@ class CreateUserForm(UserCreationForm):
         email = self.cleaned_data.get('email')
 
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('This email is invalid')
+            raise forms.ValidationError('This email is already in use')
 
-        if len(email)>=350:
-            raise forms.ValidationError("Your email is too long")
-        
         return email
-    
+
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput())
--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -11,6 +11,7 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
+from .email_server import send_email
 from .forms import CreateUserForm, LoginForm, UpdateUserForm
 from .token import user_tokenizer_generate
 
@@ -28,14 +29,18 @@ def register(request):
             user.save()
             current_site = get_current_site(request)
             subject = 'Account verification email'
-            message = render_to_string('account/registration/email-verification.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': user_tokenizer_generate.make_token(user),
-            
-            })
-            user.email_user(subject=subject, message=message)
+            send_email(
+                subject=subject,
+                to=user.email,
+                context={
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uidb64': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': user_tokenizer_generate.make_token(user),
+                },
+                template_name='account/registration/email-verification.html'
+            )
+
             return redirect('email-verification-sent')
     context = {'form':form}
     return render(request, 'account/registration/register.html', context=context)
