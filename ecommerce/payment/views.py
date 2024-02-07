564604639b83ded4fa448920f5ef6a031b8d2ed9
Vadym <petrukvadd@gmail.com>
2024-02-07 21:53:32 +0200

    deleted empty lines

--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,45 +1,20 @@
+from cart.cart import Cart
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from .models import Order, OrderItem, ShippingAddress
 
 # Create your views here.
 
-from . models import ShippingAddress, Order, OrderItem
-
-from cart.cart import Cart
-
-
-from django.http import JsonResponse
-
-
 def checkout(request):
-
-    # Users with accounts -- Pre-fill the form
-
     if request.user.is_authenticated:
-
         try:
-
-            # Authenticated users WITH shipping information 
-
             shipping_address = ShippingAddress.objects.get(user=request.user.id)
-
             context = {'shipping': shipping_address}
-
-            
-
-
             return render(request, 'payment/checkout.html', context=context)
-
-
         except:
-
-            # Authenticated users with NO shipping information
-
             return render(request, 'payment/checkout.html')
-
     else:
-            
-        # Guest users
-
         return render(request, 'payment/checkout.html')
 
 
@@ -58,38 +33,15 @@ def complete_order(request):
         state = request.POST.get('state')
         zipcode = request.POST.get('zipcode')
 
-
-        # All-in-one shipping address
-
         shipping_address = (address1 + "\n" + address2 + "\n" +
         
         city + "\n" + state + "\n" + zipcode
         
         )
 
-        # Shopping cart information 
-
         cart = Cart(request)
 
-
-        # Get the total price of items
-
         total_cost = cart.get_total()
-
-
-        '''
-
-            Order variations
-
-            1) Create order -> Account users WITH + WITHOUT shipping information
-        
-
-            2) Create order -> Guest users without an account
-        
-
-        '''
-
-        # 1) Create order -> Account users WITH + WITHOUT shipping information
 
         if request.user.is_authenticated:
 
@@ -105,9 +57,6 @@ def complete_order(request):
                 OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
                 
                 price=item['price'], user=request.user)
-
-
-        #  2) Create order -> Guest users without an account
 
         else:
 
@@ -134,46 +83,13 @@ def complete_order(request):
         return response
 
 
-
-
-
-
-    
-
-
-
-
 def payment_success(request):
-
-
-    # Clear shopping cart
-
-
     for key in list(request.session.keys()):
-
         if key == 'session_key':
-
             del request.session[key]
-
-
-
     return render(request, 'payment/payment-success.html')
-
-
-
-
-
 
 
 def payment_failed(request):
 
     return render(request, 'payment/payment-failed.html')
-
-
-
-
-
-
-
-
-
